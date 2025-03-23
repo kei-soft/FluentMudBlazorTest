0054c299e11adc2a4bbbb3bcaf637df954d5e09e
kjun <keisoft.kjun@gmail.com>
2025-03-24 00:02:06 +0900

    MudBlazor.Extensions 추가

---
+++ MudBlazorSample/Components/DatabaseInitializer.cs
@@ -0,0 +1,28 @@
+﻿using System.Data;
+
+using Dapper;
+
+namespace MudBlazorSample.Components
+{
+    public static class DatabaseInitializer
+    {
+        public static void Initialize(IDbConnection connection)
+        {
+            connection.Execute(@"
+            CREATE TABLE IF NOT EXISTS Products (
+                Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                Name TEXT NOT NULL,
+                Price REAL NOT NULL,
+                Quantity INTEGER NOT NULL
+            )");
+
+            // 샘플 데이터 삽입 -  최초 실행 후 주석 처리
+            connection.Execute(@"
+            INSERT OR IGNORE INTO Products (Name, Price, Quantity) 
+            VALUES 
+                ('Laptop', 40, 5),
+                ('Keyboard', 50, 20),
+                ('Mouse', 30, 15)");
+        }
+    }
+}
--- MudBlazorSample/Program.cs
+++ MudBlazorSample/Program.cs
@@ -1,3 +1,8 @@
+using System.Data;
+
+using Microsoft.Data.Sqlite;
+
+using MudBlazor.Extensions;
 using MudBlazor.Services;
 
 using MudBlazorSample.Components;
@@ -13,11 +18,32 @@ namespace MudBlazorSample
             // 추가
             builder.Services.AddMudServices();
 
+            // MudBlazor.Extensions
+            builder.Services.AddMudServicesWithExtensions();
+            builder.Services.AddMudExtensions();
+
             // Add services to the container.
             builder.Services.AddRazorComponents()
                 .AddInteractiveServerComponents();
 
+            // SQLite 데이터베이스 연결 추가
+            builder.Services.AddScoped<IDbConnection>((sp) =>
+            {
+                // wwwroot 폴더에 데이터베이스 파일 생성
+                var dbPath = Path.Combine(builder.Environment.WebRootPath, "products.db");
+                return new SqliteConnection($"Data Source={dbPath}");
+            });
+
             var app = builder.Build();
+
+            // SQLite 데이터베이스 초기화
+            using (var scope = app.Services.CreateScope())
+            {
+                using var connection = scope.ServiceProvider.GetRequiredService<IDbConnection>();
+                connection.Open();
+                DatabaseInitializer.Initialize(connection);
+            }
+
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
@@ -34,6 +60,9 @@ namespace MudBlazorSample
             app.MapStaticAssets();
             app.MapRazorComponents<App>()
                 .AddInteractiveServerRenderMode();
+
+            // Server
+            app.Use(MudExWebApp.MudExMiddleware);
 
             app.Run();
         }
