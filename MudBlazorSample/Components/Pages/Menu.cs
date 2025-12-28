ee30822b937e583a999c962c3261dad565e1dd1b
kjun <keisoft.kjun@gmail.com>
2025-12-28 20:51:02 +0900

    menugroup 관리

---
+++ MudBlazorSample/Components/Pages/Menu.cs
@@ -0,0 +1,18 @@
+﻿namespace MudBlazorSample.Components.Pages
+{
+    public class Menu
+    {
+        public int Id { get; set; }
+        public int? ParentId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public bool IsExpanded { get; set; } = true;
+        public List<Menu> Children { get; set; } = new();
+    }
+
+    public class MenuGroup
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public HashSet<int> MenuIds { get; set; } = new();
+    }
+}
